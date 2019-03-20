9e9dcf45ac071575da096af1a4c1b7c57250436a
Kye W. Shi <shi.kye@gmail.com>
2019-03-20 15:41:47 -0700

    add matrix tests and organize tests

--- dancinglinks_test.go
+++ dancinglinks_test.go
@@ -1,10 +1,109 @@
 package dancinglinks
 
 import (
+	"fmt"
 	"reflect"
 	"sort"
+	"strings"
 	"testing"
 )
+
+var (
+	classic = New(7, [][]int{
+		[]int{2, 4},
+		[]int{0, 3, 6},
+		[]int{1, 2, 5},
+		[]int{0, 3, 5},
+		[]int{1, 6},
+		[]int{3, 4, 6},
+	})
+	classicMatrix = [][]bool{
+		[]bool{false, false, true, false, true, false, false},
+		[]bool{true, false, false, true, false, false, true},
+		[]bool{false, true, true, false, false, true, false},
+		[]bool{true, false, false, true, false, true, false},
+		[]bool{false, true, false, false, false, false, true},
+		[]bool{false, false, false, true, true, false, true},
+	}
+	classicSolution = [][]int{
+		[]int{0, 3, 4},
+	}
+
+	classicDuplicates = New(7, [][]int{
+		[]int{2, 4},
+		[]int{2, 4},
+		[]int{0, 3, 6},
+		[]int{1, 2, 5},
+		[]int{0, 3, 5},
+		[]int{0, 3, 5},
+		[]int{1, 6},
+		[]int{3, 4, 6},
+	})
+	classicDuplicatesMatrix = [][]bool{
+		[]bool{false, false, true, false, true, false, false},
+		[]bool{false, false, true, false, true, false, false},
+		[]bool{true, false, false, true, false, false, true},
+		[]bool{false, true, true, false, false, true, false},
+		[]bool{true, false, false, true, false, true, false},
+		[]bool{true, false, false, true, false, true, false},
+		[]bool{false, true, false, false, false, false, true},
+		[]bool{false, false, false, true, true, false, true},
+	}
+	classicDuplicatesSolution = [][]int{
+		[]int{0, 4, 6},
+		[]int{0, 5, 6},
+		[]int{1, 4, 6},
+		[]int{1, 5, 6},
+	}
+
+	impossible = New(2, [][]int{
+		[]int{1},
+	})
+	impossibleMatrix = [][]bool{
+		[]bool{false, true},
+	}
+	impossibleSolution = [][]int{}
+
+	trivial         = New(0, [][]int{})
+	trivialMatrix   = [][]bool{}
+	trivialSolution = [][]int{
+		[]int{},
+	}
+)
+
+func sprintMatrix(matrix [][]bool) string {
+	b := &strings.Builder{}
+	for i, row := range matrix {
+		b.WriteString(fmt.Sprintf("%d:", i))
+		for _, cell := range row {
+			b.WriteByte(' ')
+			if cell {
+				b.WriteByte('1')
+			} else {
+				b.WriteByte('0')
+			}
+		}
+		b.WriteByte('\n')
+	}
+	return b.String()
+}
+
+func testToMatrix(t *testing.T, dl DancingLinks, expected [][]bool) {
+	mat := dl.ToMatrix()
+	if !reflect.DeepEqual(mat, expected) {
+		t.Errorf(
+			"matrix mismatch:\nshould be\n%s\ngot\n%s",
+			sprintMatrix(expected), sprintMatrix(mat),
+		)
+	}
+}
+
+func TestToMatrix(t *testing.T) {
+	testToMatrix(t, classic, classicMatrix)
+	testToMatrix(t, classicDuplicates, classicDuplicatesMatrix)
+	testToMatrix(t, impossible, impossibleMatrix)
+	testToMatrix(t, trivial, trivialMatrix)
+}
 
 func sortSequences(sequences [][]int) {
 	// First, sort each individual cover.
@@ -40,7 +139,7 @@ func sortSequences(sequences [][]int) {
 	})
 }
 
-func check(t *testing.T, dl DancingLinks, expected [][]int) {
+func testExample(t *testing.T, dl DancingLinks, expected [][]int) {
 	covers := dl.CollectSolutions()
 	sortSequences(covers)
 
@@ -49,61 +148,9 @@ func check(t *testing.T, dl DancingLinks, expected [][]int) {
 	}
 }
 
-func TestClassic(t *testing.T) {
-	check(
-		t,
-		New(7, [][]int{
-			[]int{2, 4},
-			[]int{0, 3, 4, 5, 6},
-			[]int{1, 2, 5},
-			[]int{0, 3, 5},
-			[]int{1, 6},
-			[]int{3, 4, 6},
-		}),
-		[][]int{
-			[]int{0, 3, 4},
-		},
-	)
-}
-
-func TestClassicDuplicates(t *testing.T) {
-	check(
-		t,
-		New(7, [][]int{
-			[]int{2, 4},
-			[]int{2, 4},
-			[]int{0, 3, 4, 5, 6},
-			[]int{1, 2, 5},
-			[]int{0, 3, 5},
-			[]int{0, 3, 5},
-			[]int{1, 6},
-			[]int{3, 4, 6},
-		}),
-		[][]int{
-			[]int{0, 4, 6},
-			[]int{0, 5, 6},
-			[]int{1, 4, 6},
-			[]int{1, 5, 6},
-		},
-	)
-}
-
-func TestImpossible(t *testing.T) {
-	check(
-		t,
-		New(2, [][]int{
-			[]int{1},
-		}),
-		[][]int{},
-	)
-}
-
-func TestTrivial(t *testing.T) {
-	check(
-		t,
-		New(0, [][]int{}),
-		[][]int{
-			[]int{},
-		},
-	)
+func TestExamples(t *testing.T) {
+	testExample(t, classic, classicSolution)
+	testExample(t, classicDuplicates, classicDuplicatesSolution)
+	testExample(t, impossible, impossibleSolution)
+	testExample(t, trivial, trivialSolution)
 }
